b63952f8af5dcad93450fd03bddef67eb2468ee1
Atulisboss <Atulborse2000@gmail.com>
2024-12-25 23:33:06 +0530

    Add tests for borrowing books

--- src/library.test.ts
+++ src/library.test.ts
@@ -10,3 +10,18 @@ describe('Library', () => {
     expect(library.getBooks()).to.include(book);
   });
 });
+it('should borrow a book', () => {
+    const library = new Library();
+    const book = new Book('123', 'Test Book', 'Author', 2021);
+    library.addBook(book);
+    library.borrowBook('123');
+    expect(book.isAvailable).to.be.false;
+  });
+  
+  it('should throw an error if the book is not available', () => {
+    const library = new Library();
+    const book = new Book('123', 'Test Book', 'Author', 2021, false);
+    library.addBook(book);
+    expect(() => library.borrowBook('123')).to.throw('Book not available');
+  });
+  
