6b2c257b4078438e000d9f6904392a5900a25d03
Atulisboss <Atulborse2000@gmail.com>
2024-12-25 23:40:30 +0530

    Add tests for viewing available books

--- src/library.test.ts
+++ src/library.test.ts
@@ -1,6 +1,6 @@
-import { expect } from 'chai';
 import { Library } from './library';
 import { Book } from './book';
+import { expect } from 'chai';
 
 describe('Library', () => {
   it('should add a new book', () => {
@@ -9,21 +9,22 @@ describe('Library', () => {
     library.addBook(book);
     expect(library.getBooks()).to.include(book);
   });
-});
-it('should borrow a book', () => {
+
+  it('should borrow a book', () => {
     const library = new Library();
     const book = new Book('123', 'Test Book', 'Author', 2021);
     library.addBook(book);
     library.borrowBook('123');
     expect(book.isAvailable).to.be.false;
   });
-  
+
   it('should throw an error if the book is not available', () => {
     const library = new Library();
     const book = new Book('123', 'Test Book', 'Author', 2021, false);
     library.addBook(book);
     expect(() => library.borrowBook('123')).to.throw('Book not available');
   });
+
   it('should return a book', () => {
     const library = new Library();
     const book = new Book('123', 'Test Book', 'Author', 2021, false);
@@ -31,6 +32,7 @@ it('should borrow a book', () => {
     library.returnBook('123');
     expect(book.isAvailable).to.be.true;
   });
+
   it('should view available books', () => {
     const library = new Library();
     const book1 = new Book('123', 'Test Book 1', 'Author 1', 2021, true);
@@ -41,4 +43,4 @@ it('should borrow a book', () => {
     expect(availableBooks).to.include(book1);
     expect(availableBooks).to.not.include(book2);
   });
-  
+});
